992dfddcc795d4a67abde8e7d80be5af2ac148b4
Julia Camps <julcamps@gmail.com>
2015-11-25 14:00:29 +0100

    Friedman

--- Code/Pendents/getRankMatrix.m
+++ Code/Pendents/getRankMatrix.m
@@ -1,6 +1,6 @@
-function [ output_args ] = getRankMatrix( input_args )
-% TODO
-
-
+function [ RankAverage ] = getRankAverage( ResultsMatrix )
+% ResultsMatrix contains the accuracy obtained by each algorithm used in the
+% classification task, having the datasets as rows and the algorithms as columns
+	RankAverage = mean(RsultsMatrix);
 end
 
