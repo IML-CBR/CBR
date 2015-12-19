9c5ca8e04cfffbd90d7ee26f0f5414c5b47a4e3d
Julia Camps <julcamps@gmail.com>
2015-12-19 18:19:56 +0100

    CNN

--- Code/CNN.m
+++ Code/CNN.m
@@ -1,7 +1,7 @@
 function [ U_CaseBase, U_labels ] = CNN(TrainMatrix)
     % Call CNN function
     CB=TrainMatrix(:,1:size(TrainMatrix,2)-1);
-    labels=cell2mat(TrainMatrix(:,end));
+    labels=TrainMatrix(:,end);
     U_CaseBase = [];
     U_labels = [];
     Repeat = 1;
@@ -14,14 +14,14 @@ function [ U_CaseBase, U_labels ] = CNN(TrainMatrix)
             x_case = CB(rand_order(i),:);
             y_label = labels(rand_order(i));
             [z_case, z_index] = KNN(U_CaseBase,x_case,1);
-            z_label = U_labels(z_index);
+            z_label = cell2mat(U_labels(z_index));
             if isempty(z_case)
                 U_CaseBase = [U_CaseBase; x_case];
                 U_labels = [U_labels; y_label];
                 Repeat = 1;
                 storedCases = [storedCases,rand_order(i)];
-            elseif isnumeric(y_label)
-                if y_label ~= z_label
+            elseif isnumeric(cell2mat(y_label))
+                if cell2mat(y_label) ~= z_label
                     U_CaseBase = [U_CaseBase; x_case];
                     U_labels = [U_labels; y_label];
                     Repeat = 1;
